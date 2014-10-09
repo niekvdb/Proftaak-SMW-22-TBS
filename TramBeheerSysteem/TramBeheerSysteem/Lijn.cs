6c71d6e4f29e70ce014f31961b91ff8c1195e4af
DennisKoeter <dennis.koeter@hotmail.com>
2014-10-09 14:50:40 -0700

    Lijn added

---
+++ TramBeheerSysteem/TramBeheerSysteem/Lijn.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TramBeheerSysteem
+{
+    class Lijn
+    {
+        public int Id { get; set; }
+        public Remise Remise { get; set; }
+        public int Nummer { get; set; }
+        public bool ConducteurRijdtMee { get; set; }
+
+        public Lijn(int id, Remise remise, int nummer, bool conducteurRijdtMee)
+        {
+            Id = id;
+            Remise = remise;
+            Nummer = nummer;
+            ConducteurRijdtMee = conducteurRijdtMee;
+        }
+    }
+}
