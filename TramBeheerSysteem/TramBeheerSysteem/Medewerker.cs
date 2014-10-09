b96a64eba8d98dc173256fad6e932fb73f8c9d8a
DennisKoeter <dennis.koeter@hotmail.com>
2014-10-09 14:34:53 -0700

    test

---
+++ TramBeheerSysteem/TramBeheerSysteem/Medewerker.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TramBeheerSysteem
+{
+    class Medewerker
+    {
+        public int Id { get; private set; }
+        public Functie Functie { get; private set; }
+        public string Naam { get; private set; }
+
+        public Medewerker(int id, Functie functie, string naam)
+        {
+            Id = id;
+            Functie = functie;
+            Naam = naam;
+        }
+    }
+}
