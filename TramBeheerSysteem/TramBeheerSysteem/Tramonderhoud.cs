60ccaf7c2cf0eccf3586128539cb851ef11744ed
DennisKoeter <dennis.koeter@hotmail.com>
2014-10-09 14:49:56 -0700

    tramonderhoud added

---
+++ TramBeheerSysteem/TramBeheerSysteem/Tramonderhoud.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TramBeheerSysteem
+{
+    class Tramonderhoud
+    {
+        public int Id { get; private set; }
+        public Medewerker Medewerker { get; private set; }
+        public Tram Tram { get; private set; }
+        public DateTime BeschikbaarDatum { get; private set; }
+        public string TypeOnderhoud { get; private set; }
+
+        public Tramonderhoud(int id, Medewerker medewerker, Tram tram, DateTime beschikbaarDatum, string typeOnderhoud)
+        {
+            Id = id;
+            Medewerker = medewerker;
+            Tram = tram;
+            BeschikbaarDatum = beschikbaarDatum;
+            TypeOnderhoud = typeOnderhoud;
+        }
+    }
+}
