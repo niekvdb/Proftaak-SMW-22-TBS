423a1ea23f56c170100ee607b266986f6e07757c
DennisKoeter <dennis.koeter@hotmail.com>
2014-10-09 14:25:15 -0700

    ALL OF THE CLASSES

--- TramBeheerSysteem/TramBeheerSysteem/Spoor.cs
+++ TramBeheerSysteem/TramBeheerSysteem/Spoor.cs
@@ -8,23 +8,23 @@ namespace TramBeheerSysteem
 {
     class Spoor
     {
-        public int ID { get; private set; }
-        public Remise remise { get; private set; }
-        public int nummer { get; private set; }
-        public int lengte { get; private set; }
-        public bool beschikbaar { get; private set; }
-        public bool inUitRijSpoor { get; private set; }
-        public bool blokkade { get; private set; }
+        public int Id { get; private set; }
+        public Remise Remise { get; private set; }
+        public int Nummer { get; private set; }
+        public int Lengte { get; private set; }
+        public bool Beschikbaar { get; private set; }
+        public bool InUitRijSpoor { get; private set; }
+        public bool Blokkade { get; private set; }
 
-        public Spoor (int mID, Remise mremise, int mnummer, int mlengte, bool mbeschikbaar, bool minUitRijSpoor, bool mblokkade)
+        public Spoor(int id, Remise remise, int nummer, int lengte, bool beschikbaar, bool blokkade, bool inUitRijSpoor)
         {
-            this.ID = mID;
-            this.remise = mremise;
-            this.nummer = mnummer;
-            this.lengte = mlengte;
-            this.beschikbaar = mbeschikbaar;
-            this.inUitRijSpoor = minUitRijSpoor;
-            this.blokkade = mblokkade;
+            this.Id = id;
+            this.Remise = remise;
+            this.Nummer = nummer;
+            this.Lengte = lengte;
+            this.Beschikbaar = beschikbaar;
+            this.Blokkade = blokkade;
+            this.InUitRijSpoor = inUitRijSpoor;
         }
 
         public void blokkeer()
