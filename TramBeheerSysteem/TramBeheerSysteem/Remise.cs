15a034d627823178cefc61512770e5a17ec61620
DennisKoeter <dennis.koeter@hotmail.com>
2014-10-09 14:47:57 -0700

    remise added

---
+++ TramBeheerSysteem/TramBeheerSysteem/Remise.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TramBeheerSysteem
+{
+    class Remise
+    {
+        public int Id { get; private set; }
+        public string Naam { get; private set; }
+        public int GroteServicebeurtenPerDag { get; private set; }
+        public int KleineServicebeurtenPerDag { get; private set; }
+        public int GroteSchoonmaakbeurtenPerDag { get; private set; }
+        public int KleineSchoonmaakbeurtenPerDag { get; private set; }
+
+        public Remise(string naam, int id, int groteServicebeurtenPerDag, int kleineServicebeurtenPerDag, int kleineSchoonmaakbeurtenPerDag, int groteSchoonmaakbeurtenPerDag)
+        {
+            Naam = naam;
+            Id = id;
+            GroteServicebeurtenPerDag = groteServicebeurtenPerDag;
+            KleineServicebeurtenPerDag = kleineServicebeurtenPerDag;
+            KleineSchoonmaakbeurtenPerDag = kleineSchoonmaakbeurtenPerDag;
+            GroteSchoonmaakbeurtenPerDag = groteSchoonmaakbeurtenPerDag;
+        }
+    }
+}
