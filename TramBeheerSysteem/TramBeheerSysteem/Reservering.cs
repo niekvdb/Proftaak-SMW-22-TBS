95213fc1e1e45fb5d4d379eef1e24f032fb62cbc
DennisKoeter <dennis.koeter@hotmail.com>
2014-10-09 14:51:20 -0700

    Reservering added

---
+++ TramBeheerSysteem/TramBeheerSysteem/Reservering.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TramBeheerSysteem
+{
+    class Reservering
+    {
+        public int Id { get; private set; }
+        public Tram Tram { get; private set; }
+        public Spoor Spoor { get; private set; }
+
+        public Reservering(int id, Tram tram, Spoor spoor)
+        {
+            Id = id;
+            Tram = tram;
+            Spoor = spoor;
+        }
+    }
+}
