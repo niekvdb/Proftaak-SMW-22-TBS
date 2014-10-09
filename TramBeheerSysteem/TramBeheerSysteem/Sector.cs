1938a0b7a4952806c64da481cb26079a32d2601d
DennisKoeter <dennis.koeter@hotmail.com>
2014-10-09 13:32:03 -0700

    first classes added

---
+++ TramBeheerSysteem/TramBeheerSysteem/Sector.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TramBeheerSysteem
+{
+    class Sector
+    {
+        public int ID { get; private set; }
+        public Spoor spoor { get; private set; }
+        public Tram tram { get; private set; }
+        public int nummer { get; private set; }
+        public bool beschikbaar { get; private set; }
+        public bool blokkade { get; private set; }
+
+        public Sector (int mID, Spoor mspoor, Tram mtram, int mnummer, bool mbeschikbaar, bool mblokkade)
+        {
+            this.ID = mID;
+            this.spoor = mspoor;
+            this.tram = mtram;
+            this.nummer = mnummer;
+            this.beschikbaar = mbeschikbaar;
+            this.blokkade = mblokkade;
+        }
+
+        public void blokkeer()
+        {
+            //ToDo
+        }
+    }
+}
---
+++ TramBeheerSysteem/TramBeheerSysteem/Spoor.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TramBeheerSysteem
+{
+    class Spoor
+    {
+        public int ID { get; private set; }
+        public Remise remise { get; private set; }
+        public int nummer { get; private set; }
+        public int lengte { get; private set; }
+        public bool beschikbaar { get; private set; }
+        public bool inUitRijSpoor { get; private set; }
+        public bool blokkade { get; private set; }
+
+        public Spoor (int mID, Remise mremise, int mnummer, int mlengte, bool mbeschikbaar, bool minUitRijSpoor, bool mblokkade)
+        {
+            this.ID = mID;
+            this.remise = mremise;
+            this.nummer = mnummer;
+            this.lengte = mlengte;
+            this.beschikbaar = mbeschikbaar;
+            this.inUitRijSpoor = minUitRijSpoor;
+            this.blokkade = mblokkade;
+        }
+
+        public void blokkeer()
+        {
+            //ToDo
+        }
+    }
+}
