d44e994ebac991dbd163174ccb4b48209a4e077e
niekvdb <niek_1993@hotmail.com>
2014-10-14 18:15:11 +0200

    GUI v1

--- TramBeheerSysteem/TramBeheerSysteem/SchoonMaakLijst.cs
+++ TramBeheerSysteem/TramBeheerSysteem/SchoonMaakLijst.cs
@@ -10,11 +10,16 @@ using System.Windows.Forms;
 
 namespace TramBeheerSysteem
 {
-    public partial class Form1 : Form
+    public partial class SchoonMaakLijst : Form
     {
-        public Form1()
+        public SchoonMaakLijst()
         {
             InitializeComponent();
+        }
+
+        private void dateTimePicker2_ValueChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
