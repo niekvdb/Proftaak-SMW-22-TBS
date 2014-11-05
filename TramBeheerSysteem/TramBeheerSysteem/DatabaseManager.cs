f3f4d22bd5e4661249dd5f0355b859c64f63961a
niekvdb <niek_1993@hotmail.com>
2014-11-05 10:11:28 +0100

    TramToevoegenForm

--- TramBeheerSysteem/TramBeheerSysteem/DatabaseManager.cs
+++ TramBeheerSysteem/TramBeheerSysteem/DatabaseManager.cs
@@ -5,7 +5,7 @@ using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Oracle.DataAccess.Client;
+using Oracle.DataAccess.Types;
 
 namespace TramBeheerSysteem
 {
