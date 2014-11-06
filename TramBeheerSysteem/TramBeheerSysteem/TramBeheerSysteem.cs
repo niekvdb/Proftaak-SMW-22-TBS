991d47fc710accd51d22a1a177d45f4d21bf6706
niekvdb <niek_1993@hotmail.com>
2014-11-06 16:32:59 +0100

    Tram verwijderen gefixed

--- TramBeheerSysteem/TramBeheerSysteem/TramBeheerSysteem.cs
+++ TramBeheerSysteem/TramBeheerSysteem/TramBeheerSysteem.cs
@@ -145,12 +145,21 @@ namespace TramBeheerSysteem
                 return;
             }           
             int TramNr = Convert.ToInt32(cbTrams.Text);
-            foreach (Tram t in TramManager.Trams)
+            foreach (Tram tram in TramManager.Trams)
             {
-                if (t.Id == TramNr)
-                {                    
-                    //TramManager.VerwijderTram(NR);
-                    MessageBox.Show("Tram is succesvol verwijderd");
+                if (tram.nummer == TramNr)
+                {
+                    Sector sectorCheck = RemiseManager.sectorViaTram(tram);
+                    if (sectorCheck != null)
+                    {
+                        sectorCheck.ClearSector();
+                        DatabaseManager.registreerSectorStatus(sectorCheck);
+                        MessageBox.Show("Tram is succesvol verwijderd");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Tram staat niet op een sector!");
+                    }
                 }
             }
         }
