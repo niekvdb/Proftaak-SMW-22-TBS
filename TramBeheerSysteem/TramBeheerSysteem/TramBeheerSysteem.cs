611b46538af9eadc397638ed343ba9101aeb8a4e
niekvdb <niek_1993@hotmail.com>
2014-11-06 10:37:50 +0100

    sorted

--- TramBeheerSysteem/TramBeheerSysteem/TramBeheerSysteem.cs
+++ TramBeheerSysteem/TramBeheerSysteem/TramBeheerSysteem.cs
@@ -35,6 +35,7 @@ namespace TramBeheerSysteem
             {
                 cbTrams.Items.Add(Convert.ToString(t.nummer));
             }
+            cbTrams.Sorted = true;
             foreach (Medewerker medewerker in RemiseManager.Medewerkers)
             {
                 gebruikerToolStripMenuItem.DropDownItems.Add(Convert.ToString(medewerker.Functie));
